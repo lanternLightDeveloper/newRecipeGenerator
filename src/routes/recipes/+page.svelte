dcef40c24d1712d1f3bf41f18044985c71f37b15
Joel R <joelkrivers@yahoo.com>
2026-01-23 14:05:00 -0800

    test

--- src/routes/recipes/+page.svelte
+++ src/routes/recipes/+page.svelte
@@ -7,5 +7,9 @@
 <ul>
 	{#each data.recipes as recipe}
 		<li>{recipe.name}</li>
+		<li>{recipe.servings}</li>
+		<li>{recipe.ingredients}</li>
+		<li>{recipe.instructions}</li>
+		<li>{recipe.tags}</li>
 	{/each}
 </ul>
