ac923aeeea4291bac42a0f4274c9e572db924ff3
Joel R <joelkrivers@yahoo.com>
2026-01-23 19:31:08 -0800

    ui

--- src/routes/recipes/+page.svelte
+++ src/routes/recipes/+page.svelte
@@ -5,16 +5,50 @@
 <h1>Recipes</h1>
 
 {#each data.recipes as recipe}
-	<h1>{recipe.name}</h1>
-	<ul>
-		<li>{recipe.id}</li>
-		<li>{recipe.servings}</li>
-		<li>{recipe.ingredients}</li>
-		<li>{recipe.instructions}</li>
-		<li>{recipe.tags}</li>
-		<li>{recipe.nutrition}</li>
-		<li>{recipe.time}</li>
-		<li>{recipe.creator}</li>
-		<li>{recipe.category}</li>
-	</ul>
+	<article class="card">
+		<h2>{recipe.name}</h2>
+		<p>{recipe.id}</p>
+		<p>{recipe.servings}</p>
+		<ol>
+			<li>{recipe.ingredients}</li>
+			<li>{recipe.instructions}</li>
+			<li>{recipe.tags}</li>
+			<li>{recipe.nutrition}</li>
+			<li>{recipe.time}</li>
+		</ol>
+		<p>{recipe.creator}</p>
+		<p>{recipe.category}</p>
+	</article>
 {/each}
+
+<style>
+	.card {
+		background: var(--bg-2);
+		padding: 1.5rem;
+		border-radius: 1rem;
+		border: var(--bord);
+		display: flex;
+		flex-direction: column;
+		margin: 1rem auto;
+		width: fit-content;
+		max-width: 100%;
+	}
+
+	h2 {
+		margin: 0.5rem 0;
+		padding: 0;
+		border-bottom: var(--bord);
+	}
+
+	p {
+		margin: 0.15rem 0;
+		padding: 0;
+	}
+
+	ol {
+		li {
+			margin: 0 1.25rem;
+			padding: 0;
+		}
+	}
+</style>
