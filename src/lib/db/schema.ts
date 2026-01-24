f3932e2a516088a57e004b5f0a6927038a4b84f6
Joel R <joelkrivers@yahoo.com>
2026-01-23 20:32:05 -0800

    test

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,16 +1,16 @@
-// import { pgTable, serial, integer, text } from 'drizzle-orm/pg-core';
-// import { varchar } from 'drizzle-orm/pg-core';
+import { pgTable, serial, integer, text } from 'drizzle-orm/pg-core';
+import { varchar } from 'drizzle-orm/pg-core';
 
-// export const recipes = pgTable('recipes', {
-// 	key_id: serial('key_id').primaryKey(),
-// 	id: integer('id').notNull(), // your original category-specific ID
-// 	name: text('name').notNull(),
-// 	servings: integer('servings'),
-// 	ingredients: text('ingredients').array(),
-// 	instructions: text('instructions').array(),
-// 	tags: text('tags').array(),
-// 	nutrition: text('nutrition'),
-// 	time: integer('time'),
-// 	creator: text('creator').default('Unknown'),
-// 	category: text('category').notNull()
-// });
+export const recipes = pgTable('recipes', {
+	key_id: serial('key_id').primaryKey(),
+	id: integer('id').notNull(), // your original category-specific ID
+	name: text('name').notNull(),
+	servings: integer('servings'),
+	ingredients: text('ingredients').array(),
+	instructions: text('instructions').array(),
+	tags: text('tags').array(),
+	nutrition: text('nutrition'),
+	time: integer('time'),
+	creator: text('creator').default('Unknown'),
+	category: text('category').notNull()
+});
