f9e3b50004a1400ade3033f1eaf615abe157952d
Joel R <joelkrivers@yahoo.com>
2026-02-03 19:34:11 -0800

    test

--- src/routes/profiles/admin/+page.server.ts
+++ src/routes/profiles/admin/+page.server.ts
@@ -1,9 +1,9 @@
-import { error } from '@sveltejs/kit';
+// import { error } from '@sveltejs/kit';
 
-export const load = ({ locals }) => {
-	if (!locals.user.groups.includes('admin')) {
-		throw error(403, 'Admins only');
-	}
+// export const load = ({ locals }) => {
+// 	if (!locals.user.groups.includes('admin')) {
+// 		throw error(403, 'Admins only');
+// 	}
 
-	return { user: locals.user };
-};
+// 	return { user: locals.user };
+// };
--- src/routes/profiles/admin/+page.svelte
+++ src/routes/profiles/admin/+page.svelte
@@ -1,1 +1,1 @@
-<h3>This really is an admin space</h3>
+<!-- <h3>This really is an admin space</h3> -->
