870b253a5686545c2d485ed275b38257cf6b5059
Joel R <joelkrivers@yahoo.com>
2026-02-04 12:37:41 -0800

    test

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -2,7 +2,7 @@ import { redirect } from '@sveltejs/kit';
 
 export const load = async ({ locals }) => {
 	if (!locals.user) {
-		throw redirect(302, '/login');
+		throw redirect(302, '/');
 	}
 
 	return {
