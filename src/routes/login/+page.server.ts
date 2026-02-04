ba4977f7c35a0cce78f9baae9bd93581c7411bfa
Joel R <joelkrivers@yahoo.com>
2026-02-03 19:11:44 -0800

    test

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,11 +1,12 @@
 import { redirect } from '@sveltejs/kit';
-import { generators } from 'openid-client';
+import crypto from 'crypto';
 
 export const GET = async () => {
-	const state = generators.state();
-	const url = new URL(process.env.AUTHENTIK_AUTHORIZE_URL);
-	url.searchParams.set('client_id', process.env.AUTHENTIK_CLIENT_ID);
-	url.searchParams.set('redirect_uri', process.env.AUTHENTIK_REDIRECT_URI);
+	const state = crypto.randomBytes(16).toString('hex');
+
+	const url = new URL(process.env.AUTHENTIK_AUTHORIZE_URL!);
+	url.searchParams.set('client_id', process.env.AUTHENTIK_CLIENT_ID!);
+	url.searchParams.set('redirect_uri', process.env.AUTHENTIK_REDIRECT_URI!);
 	url.searchParams.set('response_type', 'code');
 	url.searchParams.set('scope', 'openid profile email');
 	url.searchParams.set('state', state);
