c42ccf3efa7683ff2708e8a03a1b757ec6ea8a3c
Joel R <joelkrivers@yahoo.com>
2026-02-05 21:17:08 -0800

    cleaning

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -2,15 +2,14 @@
 	let { children } = $props();
 </script>
 
-<div class="app">
-	<main>
-		{@render children()}
-	</main>
-</div>
-
 <ul>
 	<li><a href="/admin/users">Manage users</a></li>
 	<li><a href="/admin/recipes">Manage recipes</a></li>
 	<li><a href="/admin/settings">Manage Settings</a></li>
 	<li><a href="/admin/password-resets">Pending Password Resets</a></li>
 </ul>
+<div class="app">
+	<main>
+		{@render children()}
+	</main>
+</div>
